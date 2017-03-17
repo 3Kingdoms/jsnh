969ca3d29fbb4c0cac18dcdef391638b704ed4c7
Jason HU Shengjie <jasonhu515@gmail.com>
2017-03-17 15:05:41 +0800

    adding scripts, update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jsnh
+
+Major modified files:
+* bsSpaz.py
+* bsBomb.py
+* bsPowerup.py
+
+Modified parts are usually with comment markers,  
+`# jasonhu5` as start and ends with `#`.
---
+++ sys/1.4.105/flaplandLevelDefs.py
@@ -0,0 +1,12 @@
+# This file generated from 'flapland.blend'
+points, boxes = {}, {}
+points['powerupSpawn1'] = (3.36871, 3.98352, -5.47726)
+points['powerupSpawn2'] = (-4.26556, 3.36587, -2.63607)
+points['powerupSpawn3'] = (3.21753, 3.86432, -2.55631)
+points['powerupSpawn4'] = (-4.21771, 3.48507, -5.40329)
+points['shadowLowerBottom'] = (0.03819, -0.86597, 3.36804)
+points['shadowLowerTop'] = (0.03819, 0.06026, 3.36804)
+points['shadowUpperBottom'] = (0.03819, 4.77523, 3.36804)
+points['shadowUpperTop'] = (0.03819, 7.25347, 3.36804)
+boxes['areaOfInterestBounds'] = (0.46773, 2.23555, -3.2698) + (0, 0, 0) + (17.68531, 8.4954, 12.15497)
+boxes['levelBounds'] = (0.4577, 1.41685, -3.75614) + (0, 0, 0) + (22.98847, 11.96635, 19.04444)
